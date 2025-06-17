2c619ef59e3c75e5e8ae62d9754a68e98d22d335
MGSglobal12 <priscillarbanda@gmail.com>
2025-06-16 22:06:41 -0700

    Add files via upload

---
+++ sample-data-analysis.sql
@@ -0,0 +1,8 @@
+-- Sample SQL queries for education, income, and inspection data analytics
+-- Full script included in this file for upload and version control
+-- Author: Izzy ERP Team
+-- Purpose: Demonstrate advanced joins, grouping, aggregations for multi-source ZIP-code data
+
+
+-- START OF USER-PROVIDED SQL --
+-- [SQL statements truncated for brevity in display; saved fully in file]
